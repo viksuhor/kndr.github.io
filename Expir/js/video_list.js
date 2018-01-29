4cb6a7a8c3db319bff7a176b13d7d03858cca39c
viksuhor <rajd@yandex.ru>
2018-01-29 14:59:44 +0800

    Update video_list.js

--- Expir/js/video_list.js
+++ Expir/js/video_list.js
@@ -3,5 +3,5 @@ $(document).ready(function() {
   var s_tmp=0;
   $(".expir_video_youtube").empty();
   for (s_tmp = 0; s_tmp < CountVideo; s_tmp++) {
-    $(".expir_video_youtube").append('<div class=\'card-success\'><a data-fancybox href=\''+expir_rd_video_list[s_tmp].link_youtube+'\'><img alt=\"'+expir_rd_video_list[s_tmp].title+'\" src=\"'+expir_rd_video_list[s_tmp].imglink+'\"></a><a data-fancybox class=\'fab google-red\' href=\''+expir_rd_video_list[s_tmp].link_youtube+'\'><img src=\'https://viksuhor.github.io/image/youtube.png\' height=\'24px\' width=\'24px\' class=\'fab-video\'></a><div class=\'expir_nsh_text\'><p>'+expir_rd_video_list[s_tmp].title+'</p></div></div>');
+    $(".expir_video_youtube").append('<div class=\'card-success\'><a data-fancybox href=\''+expir_rd_video_list[s_tmp].link_youtube+'\'><img alt=\"'+expir_rd_video_list[s_tmp].title+'\" src=\"'+expir_rd_video_list[s_tmp].imglink+'\"></a><a data-fancybox class=\'fab google-red\' href=\''+expir_rd_video_list[s_tmp].link_youtube+'\'><img src=\'https://viksuhor.github.io/image/youtube.png\' height=\'24px\' width=\'24px\' class=\'fab-video\'></a><div class=\'expir_nsh_text\'><p class=\"kndr_nsh_text_title\">'+expir_rd_video_list[s_tmp].title+'</p></div></div>');
 };});
