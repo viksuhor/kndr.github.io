fd7120b8708f113274f5a84ab276013ce71fff6e
viksuhor <rajd@yandex.ru>
2018-01-29 13:39:18 +0800

    Add files via upload

---
+++ Expir/js/video_list.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+ var CountVideo = expir_rd_video_list.length;
+  var s_tmp=0;
+  $(".expir_video_youtube").empty();
+  for (s_tmp = 0; s_tmp < CountVideo; s_tmp++) {
+    $(".expir_video_youtube").append('<div class=\'card-success\'><a data-fancybox href=\''+expir_rd_video_list[s_tmp].link_youtube+'\'><img alt=\"'+expir_rd_video_list[s_tmp].title+'\" src=\"'+expir_rd_video_list[s_tmp].imglink+'\"></a><a data-fancybox class=\'fab google-red\' href=\''+expir_rd_video_list[s_tmp].link_youtube+'\'><img src=\'https://viksuhor.github.io/image/youtube.png\' height=\'24px\' width=\'24px\' class=\'icon-fab-video\'></a><div class=\'expir_nsh_text\'><h2>'+expir_rd_video_list[s_tmp].title+'</h2></div></div>');
+};});
